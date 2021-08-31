4dcf8da3fdff0ce6f7a86bc860ab49ea3d890fb0
Garima Saraswat <39646652+garimasaraswat@users.noreply.github.com>
2021-08-31 14:31:09 +0200

    more data files analysed

--- extract_lat.py
+++ extract_lat.py
@@ -7,17 +7,19 @@ from scipy.spatial import KDTree
 
 def lat_con(topofile='Data/18d03T0015.txt',thrs=0.2,pxdist=3):
     data=np.loadtxt(topofile)
-    data_z=data*1e9
+    data_z=data/np.mean(data)
     xy = peak_local_max(data_z, min_distance=pxdist,threshold_abs=thrs)
     tree=KDTree(xy)
-    dd,ii=tree.query(xy,k=2)
-    a,b=np.histogram(dd[:,1],bins=30)
-    c=b[np.where(a==max(a))][0]
+    dd,ii=tree.query(xy,k=4)
+    #a,b=np.histogram(dd[:,1],bins=30)
+    #c=b[np.where(a==max(a))][0]
     imsize= [float(x.strip(' "')) for x in re.findall("\d+\.\d+", linecache.getline(topofile, 2))][0]
     pxsize=imsize/data_z.shape[0]
-    latcon=c*pxsize
+    nn1=dd[:,1]*pxsize
+    nn2=dd[:,2]*pxsize
+    nn3=dd[:,3]*pxsize
     data_x=np.linspace(0, imsize, num=data_z.shape[0])
     data_y=data_x
     X,Y=np.meshgrid(data_x,data_y)
     xynm=xy*pxsize
-    return latcon, X,Y,data_z, xynm
+    return nn1,nn2,nn3, X,Y,data_z, xynm
